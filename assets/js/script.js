ac6f20606a608115c5c10ed12855e36f13f5c94b
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-03-24 17:35:02 +0000

    add all

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,10 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     }
 
+    else if (gameType === "Division") {
+        displayDivisionQuestion(num1, num2);
+    }
+
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -135,7 +139,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand1').textContent = operand2 > operand2 ? operand2 : operand1;
+    document.getElementById('operand2').textContent = operand2 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
@@ -145,7 +149,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";    
 
 }
 
