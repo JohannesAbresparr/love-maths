29808c099f118d77bb3fb57074017f7644a8fe21
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-03-24 10:40:44 +0000

    correct typo fuMction to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to load before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", fumction() {
+document.addEventListener("DOMContentLoaded", function() {
 let buttons = document.getElementsByTagName("button");
 for(let button of buttons){
     button.addEventListener("click", function(){
