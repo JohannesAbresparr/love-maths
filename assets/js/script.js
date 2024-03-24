370ca736fb10eb1543d409bdc6b005b3a3bd3585
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-03-24 10:55:35 +0000

    Add math function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,13 @@ for(let button of buttons){
 
 })
 
-
-
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -39,6 +41,7 @@ function incrementScore() {
 function incrementWrongAnswer() {
     
 }
+
 
 function displayAdditionQuestion() {
     
