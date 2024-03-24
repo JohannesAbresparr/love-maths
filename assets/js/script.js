ea46ab7ac7c4f28a023269983299e08f082f13e1
Johannes Abresparr <johannes.abresparr@gmail.com>
2024-03-24 14:21:10 +0000

    keep score of the changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww... your answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 
@@ -73,11 +75,24 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the correct score from the som and inkremens it by 1
+ */
+
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the currrent tally of incorrect answers from the DOM and increments it by 1
+ */
+
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
